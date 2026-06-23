d054108b13254d7a055ebe07bc8834c358dd149c
Saint-J-Franklin <support@spherix.io>
2026-06-23 13:59:50 +0200

    Add Meta Pixel (ID 995386490060228) to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { CartProvider } from '@/context/CartContext'
 import Header from '@/components/Header'
@@ -22,6 +23,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="bg" className={`${inter.variable} overflow-x-hidden`}>
       <body className="min-h-screen flex flex-col overflow-x-hidden w-full">
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '995386490060228');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=995386490060228&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <CartProvider>
           <Header />
           <div className="flex-1 w-full overflow-x-hidden">{children}</div>
